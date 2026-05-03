4e34559decffe12a5ad60da6ffce1ca07c466ec6
Gloria <gloria.duo@outlook.com>
2026-05-03 10:18:47 -0700

    Implemented Hough Transform.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "gaussian.h"
 #include "canny.h"
-#include <stdio.h>
+#include "hough.h"
 
 int main(int argc, const char * argv[]) {
        // Usage: ./main gaussian input_path output_path kernel_size sigma
@@ -45,6 +45,26 @@ int main(int argc, const char * argv[]) {
               deleteImage(outputImage);
               deleteMatrix(inputMatrix);
               deleteMatrix(canny);
+       // Usage: ./main hough input_path output_path g_size g_sigma canny_low canny_high r_min r_max vote_threshold
+       } else if (strcmp(argv[1], "hough") == 0) {
+              if (argc != 11) {
+                     fprintf(stderr, "Usage: %s hough input.ppm output.ppm g_size g_sigma canny_low canny_high r_min r_max arc_pct\n"
+                                     "  arc_pct: minimum %% of circle circumference that must be an edge (0-100)\n", argv[0]);
+                     return 1;
+              }
+              Image inputImage   = readImage((char *)argv[2]);
+              Matrix inputMatrix = image2Matrix(inputImage);
+              Matrix blurred     = gaussianBlur(inputMatrix, atoi(argv[4]), atof(argv[5]));
+              Matrix edges       = cannyEdgeDetection(blurred, atoi(argv[6]), atoi(argv[7]));
+              Image outputImage  = houghCircleDetection(edges, inputImage,
+                                                        atoi(argv[8]), atoi(argv[9]), atoi(argv[10]));
+              writeImage(outputImage, (char *)argv[3]);
+              deleteMatrix(inputMatrix);
+              deleteMatrix(blurred);
+              deleteMatrix(edges);
+              deleteImage(inputImage);
+              deleteImage(outputImage);
+
        } else {
               fprintf(stderr, "Unknown filter: %s\n", argv[1]);
               return 1;
